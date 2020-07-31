cffad5e6e69f518283bd5f9b28f68c4d6aefc02d
alfhad <fahadshah2411@gmail.com>
2020-08-01 00:03:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-    var s = "<h1>Sitaaron Ke Aage Jahaan Aur Bhi Hain.</h1><h1>Abhi Ishq Ke Imtihaan Aur Bhi Hain</h1>"
+    var s = "<h1>Sitaaron Ke Aage Jahaan Aur Bhi Hain.</h1><h1>Abhi Ishq Ke Imtihaan Aur Bhi Hain</h1><h1>Abhi Hum Zinda Hai !!"
   res.end(s);
 });
 
