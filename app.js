c894981a2642f4a65d701862e6312dc4cb00e1a9
Devang K <52554056+rising-entropy@users.noreply.github.com>
2020-07-31 23:42:38 +0530

    qwertyuio

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
-const http = require('http');
-const fs = require("fs");
-const path = require("path");
+var http = require('http'),
+    fs = require('fs');
 
-function send404(response){
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: Resource not found.');
-  response.end();
-}
 
-const mimeLookup = {
-  '.js': 'application/javascript',
-  '.html': 'text/html'
-};
-
-const server = http.createServer((req, res) => {
-  if(req.method == 'GET'){
-
-    let fileurl;
-    if(req.url == '/'){
-      fileurl = 'index.html';
-    }else{
-      fileurl = req.url;
-    }
-    let filepath = path.resolve('./' + fileurl);
-
-    let fileExt = path.extname(filepath);
-    let mimeType = mimeLookup[fileExt];
-
-    if(!mimeType) {
-      send404(res);
-      return;
-    }
-
-    fs.exists(filepath, (exists) => {
-      if(!exists){
-        send404(res);
-        return;
-      }
-
-      res.writeHead(200, {'Content-Type': mimeType});
-      fs.createReadStream(filepath).pipe(res);
-
-    });
-
-  }
-}).listen(3000);
-console.log("Server running at port 3000");
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
