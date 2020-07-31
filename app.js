c8bbdd7711aa32e0877472b3a11eb8f589627df0
Devang K <52554056+rising-entropy@users.noreply.github.com>
2020-07-31 23:28:05 +0530

    qwertyui

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Devaiyya</h1>');
+  res.sendFile(__dirname + "index.html");
 });
 
 server.listen(port,() => {
