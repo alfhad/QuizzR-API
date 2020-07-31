bfb2e72cc7fd1ed802835852b02f519ab3da1d5a
Devang K <52554056+rising-entropy@users.noreply.github.com>
2020-07-31 23:45:08 +0530

    app.js deployment

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var http = require('http'),
-    fs = require('fs');
+const http = require('http');
+const port = process.env.PORT || 3000
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+    var s = "<h1>Sitaaron Ke Aage Jahaan Aur Bhi Hain.</h1><h1>Abhi Ishq Ke Imtihaan Aur Bhi Hain</h1>"
+  res.end(s);
+});
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
